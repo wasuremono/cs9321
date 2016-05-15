eb8e7b074adce651398b6b03b52f0e7d59c378ef
wasuremono <kenneth.s.k.ng@gmail.com>
2016-05-15 12:05:47 +1000

    Fixed room availability checking

--- src/hotel/chain/CheckoutServlet.java
+++ src/hotel/chain/CheckoutServlet.java
@@ -2,17 +2,21 @@ package hotel.chain;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.util.Vector;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import beans.BookingBean;
 import beans.UserBean;
 
 /**
@@ -47,30 +51,55 @@ public class CheckoutServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		UserBean u = (UserBean) request.getSession().getAttribute("userBean");
+		Vector<Cart> userCart = new Vector<Cart>();
 		boolean bookingOpen = true;
 		//Book Rooms
 		try {
 			Connection conn = DatabaseTool.getConnection();
 			//for each userbookingorder
 			//sum up number of each roomtype
-			
+			conn = DatabaseTool.getConnection();
+			PreparedStatement ps = conn.prepareStatement("select * from bookingOrders where uid =?;");
+			//change this to bookingid
+			ps.setInt(1,u.getId());
+			ResultSet rs = ps.executeQuery();
+			if(!rs.next()){
+				System.out.println("No Bookings for selected user");
+			} else {
+				rs.beforeFirst();
+				while(rs.next()){
+					Cart c = new Cart();
+					c.parseResultSet(rs);
+					userCart.add(c);
+				}	
+			}
+			for(Cart b:userCart){
+				//select booking id
+				//check for roomType and checkin/out Date
+				//check availability
+				//if any rooms are unavailable->return to cart
+			//}
 			//TODO: Check for rooms based on which hotel they belong to
 			//for each roomtype:
-			for(RoomType rt:RoomType.values()){
+			//for(RoomType rt:RoomType.values()){
 				int userBookingCount = 0;
 				int totalBookingCount = 0;
 				int totalRooms = 0;
 				//count user bookings for this roomtype
-				PreparedStatement ps = conn.prepareStatement("SELECT COUNT(*) as numRooms from bookingorders where roomType = ? AND uid = ? ;");
-				ps.setString(1, rt.toString());
+				ps = conn.prepareStatement("SELECT COUNT(*) as numRooms from bookingorders where roomType = ? AND uid = ? AND checkin >=? and checkout <= ?;");
+				ps.setString(1, b.getRoomType());
 				ps.setInt(2, u.getId());
-				ResultSet rs = ps.executeQuery();
+				ps.setDate(3, (Date) b.getCheckout());
+				ps.setDate(4, (Date) b.getCheckin());
+				rs = ps.executeQuery();
 				if(rs.next()){
 					userBookingCount = rs.getInt("numRooms");
 				}
 				//count all bookings for this roomtype
-				ps = conn.prepareStatement("SELECT COUNT(*) as numRooms from bookings where roomType = ?;");
+				ps = conn.prepareStatement("SELECT COUNT(*) as numRooms from bookings where roomType = ? AND checkin >=? and checkout <= ?;");
 				rs = ps.executeQuery();
+				ps.setDate(1, (Date) b.getCheckout());
+				ps.setDate(2, (Date) b.getCheckin());
 				if(rs.next()){
 					totalBookingCount = rs.getInt("numRooms");
 				}
@@ -78,6 +107,7 @@ public class CheckoutServlet extends HttpServlet {
 				
 				ps = conn.prepareStatement("SELECT COUNT(*) as numRooms from rooms where roomType = ?;");
 				rs = ps.executeQuery();
+				
 				if(rs.next()){
 					totalRooms = rs.getInt("numRooms");
 				}
@@ -87,9 +117,16 @@ public class CheckoutServlet extends HttpServlet {
 					break;
 				}
 			}
-			//Move from bookingorder to booking
+			
+			if(!bookingOpen){
+				RequestDispatcher rd = request.getRequestDispatcher("/cart.jsp");
+				String bookingError = "One or more of the selected rooms are currently unavailable, please try again at a later time or cancel current booking";
+				request.setAttribute("bookingError",bookingError );
+				return;
+			}
+			//Move from bookingorder to booking if all fulfilled
 			if(bookingOpen){
-				PreparedStatement ps = conn.prepareStatement("UPDATE bookingOrders SET bookingDate = CURDATE() where uid = ? ;");
+				ps = conn.prepareStatement("UPDATE bookingOrders SET bookingDate = CURDATE() where uid = ? ;");
 				ps.setInt(1, u.getId());
 				ps.executeUpdate();		
 				ps = conn.prepareStatement("Insert into bookings select * from bookingorders where uid = ?;");
@@ -101,7 +138,7 @@ public class CheckoutServlet extends HttpServlet {
 			}
 			DatabaseTool.endConnection(conn);
 			//return
-		} catch (SQLException | InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+		} catch (SQLException | InstantiationException | IllegalAccessException | ClassNotFoundException | ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
